5a8ae7428da40d623d2afe593c53d844f0309193
srivastavanik <nicksriv@outlook.com>
2025-03-14 12:34:28 -0700

    Delete extras

--- NFT-Appraisal-Main-main/Backend/Ai/flare_ai_consensus/api/__init__.py
+++
@@ -1,3 +0,0 @@
-from .routes.chat import ChatMessage, ChatRouter, router
-
-__all__ = ["ChatMessage", "ChatRouter", "router"]
--- NFT-Appraisal-Main-main/Backend/Ai/functions/flare_ai_consensus/router/__init__.py
+++
@@ -1,9 +0,0 @@
-from .base_router import ChatRequest, CompletionRequest
-from .openrouter import AsyncOpenRouterProvider, OpenRouterProvider
-
-__all__ = [
-    "AsyncOpenRouterProvider",
-    "ChatRequest",
-    "CompletionRequest",
-    "OpenRouterProvider",
-]
